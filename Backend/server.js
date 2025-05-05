e6139f675d214ad8409c5e5dff296b8cf0fba10f
ANU-2524 <anusoni25.2006@gmail.com>
2025-05-05 23:34:16 +0530

    Created server.js

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require("mongoose")
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection !
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("MongoDB connected !"))
+.catch((err)=>console.log("Error occurred during Connection !"))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
