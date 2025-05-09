cb1520e837cfbde333fbf4c52a43e5aa90bcf0f8
ANU-2524 <anusoni25.2006@gmail.com>
2025-05-09 17:03:58 +0530

    Updated

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -18,8 +18,8 @@ router.post('/signup', async (req, res) => {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: 'User already exists' });
     const newUser = await User.create({ name, email, password });
-    const token = jwt.sign({ id: newUser._id }, JWT_SECRET, { expiresIn: '1d' });
-    res.status(201).json({ token, user: { id: newUser._id, name: newUser.name } });
+    // const token = jwt.sign({ id: newUser._id }, JWT_SECRET, { expiresIn: '1d' });
+    res.status(201).json({user: { id: newUser._id, name: newUser.name } });
   } catch (err) {
     res.status(500).json({ message: 'Signup failed' });
   }
@@ -30,7 +30,7 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+    if (!user) return res.status(400).json({ message: 'User must signup first.' });
     const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(400).json({ message: 'Invalid credentials' });
     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1d' });
