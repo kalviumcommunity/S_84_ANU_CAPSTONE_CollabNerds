165ecfcc46ddcc7fa05b6ba8a007c4358d37a238
ANU-2524 <anusoni25.2006@gmail.com>
2025-05-12 15:06:40 +0530

    Created upload.js

---
+++ Backend/middleware/upload.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/profile_images/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+      return cb(new Error('Only images are allowed'), false);
+    }
+    cb(null, true);
+  },
+});
+
+module.exports = upload;
