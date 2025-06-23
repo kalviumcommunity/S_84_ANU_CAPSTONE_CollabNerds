ce7a634ad351d354e83e336d0c729d01911d01fc
ANU-2524 <anusoni25.2006@gmail.com>
2025-06-23 17:19:36 +0530

    Updated

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 // === CORS Configuration ===
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://gregarious-marzipan-0e011f.netlify.app', 
+  'https://glowing-melba-4fb0ac.netlify.app', 
 ];
 
 app.use(cors({
